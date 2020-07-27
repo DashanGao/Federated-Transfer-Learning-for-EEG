b4907eb64b200cdbb01168c7e11f265f89842c03
Dashan Gao <dashan.gao@connect.ust.hk>
2020-07-27 17:20:35 +0800

    edit

--- MMD_loss.py
+++ MMD_loss.py
@@ -19,13 +19,13 @@ class MMD_loss(nn.Module):
             int(total.size(0)), int(total.size(0)), int(total.size(1)))
         total1 = total.unsqueeze(1).expand(
             int(total.size(0)), int(total.size(0)), int(total.size(1)))
-        L2_distance = ((total0-total1)**2).sum(2)
+        L2_distance = ((total0 - total1) ** 2).sum(2)
         if fix_sigma:
             bandwidth = fix_sigma
         else:
-            bandwidth = torch.sum(L2_distance.data) / (n_samples**2-n_samples)
+            bandwidth = torch.sum(L2_distance.data) / (n_samples ** 2 - n_samples)
         bandwidth /= kernel_mul ** (kernel_num // 2)
-        bandwidth_list = [bandwidth * (kernel_mul**i)
+        bandwidth_list = [bandwidth * (kernel_mul ** i)
                           for i in range(kernel_num)]
         kernel_val = [torch.exp(-L2_distance / bandwidth_temp)
                       for bandwidth_temp in bandwidth_list]
--- model.py
+++ model.py
@@ -1,33 +1,31 @@
 import torch
 from torch.autograd import Variable
-import scipy.io as sio
-import numpy as np
 import spd_net_util as util
-import torch.nn as nn
+
 
 class SPDNetwork(torch.nn.Module):
 
     def __init__(self):
         super(SPDNetwork, self).__init__()
-        #tmp = sio.loadmat('./tmp/afew/w_1.mat')['w_1']
-        #self.w_1_p = Variable(torch.from_numpy(tmp), requires_grad=True)
+        # tmp = sio.loadmat('./tmp/afew/w_1.mat')['w_1']
+        # self.w_1_p = Variable(torch.from_numpy(tmp), requires_grad=True)
         self.w_1_p = Variable(torch.randn(32, 4).double(), requires_grad=True)
-        #print('w1.shape: ', tmp.shape)
+        # print('w1.shape: ', tmp.shape)
 
-        #tmp = sio.loadmat('./tmp/afew/w_2.mat')['w_2']
-        #self.w_2_p = Variable(torch.from_numpy(tmp), requires_grad=True)
+        # tmp = sio.loadmat('./tmp/afew/w_2.mat')['w_2']
+        # self.w_2_p = Variable(torch.from_numpy(tmp), requires_grad=True)
         self.w_2_p = Variable(torch.randn(4, 4).double(), requires_grad=True)
-        #print('w2.shape: ', tmp.shape)
+        # print('w2.shape: ', tmp.shape)
 
-        #tmp = sio.loadmat('./tmp/afew/w_3.mat')['w_3']
-        #self.w_3_p = Variable(torch.from_numpy(tmp), requires_grad=True)
+        # tmp = sio.loadmat('./tmp/afew/w_3.mat')['w_3']
+        # self.w_3_p = Variable(torch.from_numpy(tmp), requires_grad=True)
         self.w_3_p = Variable(torch.randn(4, 4).double(), requires_grad=True)
-        #print('w3.shape: ', tmp.shape)
+        # print('w3.shape: ', tmp.shape)
 
-        #tmp = sio.loadmat('./tmp/afew/fc.mat')['theta']
-        #self.fc_w = Variable(torch.from_numpy(tmp.astype(np.float64)), requires_grad=True)
+        # tmp = sio.loadmat('./tmp/afew/fc.mat')['theta']
+        # self.fc_w = Variable(torch.from_numpy(tmp.astype(np.float64)), requires_grad=True)
         self.fc_w = Variable(torch.randn(16, 2).double(), requires_grad=True)
-        #print('fc.shape: ', tmp.shape)
+        # print('fc.shape: ', tmp.shape)
 
     def forward(self, input):
         batch_size = input.shape[0]
@@ -58,7 +56,6 @@ class SPDNetwork(torch.nn.Module):
         return logits
 
     def update_para(self, lr):
-
         egrad_w1 = self.w_1_p.grad.data.numpy()
         egrad_w2 = self.w_2_p.grad.data.numpy()
         egrad_w3 = self.w_3_p.grad.data.numpy()
@@ -93,17 +90,15 @@ class SPDNetwork_1(torch.nn.Module):
     def __init__(self):
         super(SPDNetwork_1, self).__init__()
 
-
         self.w_1_p = Variable(torch.randn(32, 32).double(), requires_grad=True)
         self.w_2_p = Variable(torch.randn(32, 16).double(), requires_grad=True)
         self.w_3_p = Variable(torch.randn(16, 4).double(), requires_grad=True)
         self.fc_w = Variable(torch.randn(16, 2).double(), requires_grad=True)
 
-        #self.w_1_p = Variable(nn.init.zeros_(torch.empty(32,16)).double(), requires_grad=True)
-        #self.w_2_p = Variable(nn.init.zeros_(torch.empty(16,8)).double(), requires_grad=True)
-        #self.w_3_p = Variable(nn.init.eye_(torch.empty(8,4)).double(), requires_grad=True)
-        #self.fc_w = Variable(nn.init.zeros_(torch.empty(16,2)).double(), requires_grad=True)
-
+        # self.w_1_p = Variable(nn.init.zeros_(torch.empty(32,16)).double(), requires_grad=True)
+        # self.w_2_p = Variable(nn.init.zeros_(torch.empty(16,8)).double(), requires_grad=True)
+        # self.w_3_p = Variable(nn.init.eye_(torch.empty(8,4)).double(), requires_grad=True)
+        # self.fc_w = Variable(nn.init.zeros_(torch.empty(16,2)).double(), requires_grad=True)
 
     def forward(self, input):
         batch_size = input.shape[0]
@@ -119,12 +114,12 @@ class SPDNetwork_1(torch.nn.Module):
         w_tX = torch.matmul(torch.transpose(w_1, dim0=1, dim1=2), input)
         w_tXw = torch.matmul(w_tX, w_1)
         X_1 = util.rec_mat_v2(w_tXw)
-        #X_1 = w_tXw
+        # X_1 = w_tXw
 
         w_tX = torch.matmul(torch.transpose(w_2, dim0=1, dim1=2), X_1)
         w_tXw = torch.matmul(w_tX, w_2)
         X_2 = util.rec_mat_v2(w_tXw)
-        #X_2 = w_tXw
+        # X_2 = w_tXw
 
         w_tX = torch.matmul(torch.transpose(w_3, dim0=1, dim1=2), X_2)
         w_tXw = torch.matmul(w_tX, w_3)
@@ -136,7 +131,6 @@ class SPDNetwork_1(torch.nn.Module):
         return logits, feat
 
     def update_para(self, lr):
-
         egrad_w1 = self.w_1_p.grad.data.numpy()
         egrad_w2 = self.w_2_p.grad.data.numpy()
         egrad_w3 = self.w_3_p.grad.data.numpy()
@@ -152,7 +146,7 @@ class SPDNetwork_1(torch.nn.Module):
         self.w_2_p.data.copy_(torch.DoubleTensor(new_w_2))
         self.w_3_p.data.copy_(torch.DoubleTensor(new_w_3))
 
-        #self.fc_w.data -= lr * self.fc_w.grad.data
+        # self.fc_w.data -= lr * self.fc_w.grad.data
         # Manually zero the gradients after updating weights
         self.w_1_p.grad.data.zero_()
         self.w_2_p.grad.data.zero_()
@@ -169,19 +163,12 @@ class SPDNetwork_2(torch.nn.Module):
 
     def __init__(self):
         super(SPDNetwork_2, self).__init__()
-
 
         self.w_1_p = Variable(torch.randn(32, 4).double(), requires_grad=True)
         self.w_2_p = Variable(torch.randn(4, 4).double(), requires_grad=True)
         self.w_3_p = Variable(torch.randn(4, 4).double(), requires_grad=True)
         self.fc_w = Variable(torch.randn(16, 2).double(), requires_grad=True)
 
-        #self.w_1_p = Variable(nn.init.zeros_(torch.empty(32,16)).double(), requires_grad=True)
-        #self.w_2_p = Variable(nn.init.zeros_(torch.empty(16,8)).double(), requires_grad=True)
-        #self.w_3_p = Variable(nn.init.eye_(torch.empty(8,4)).double(), requires_grad=True)
-        #self.fc_w = Variable(nn.init.zeros_(torch.empty(16,2)).double(), requires_grad=True)
-
-
     def forward(self, input):
         batch_size = input.shape[0]
         w_1_pc = self.w_1_p.contiguous()
@@ -196,12 +183,12 @@ class SPDNetwork_2(torch.nn.Module):
         w_tX = torch.matmul(torch.transpose(w_1, dim0=1, dim1=2), input)
         w_tXw = torch.matmul(w_tX, w_1)
         X_1 = util.rec_mat_v2(w_tXw)
-        #X_1 = w_tXw
+        # X_1 = w_tXw
 
         w_tX = torch.matmul(torch.transpose(w_2, dim0=1, dim1=2), X_1)
         w_tXw = torch.matmul(w_tX, w_2)
         X_2 = util.rec_mat_v2(w_tXw)
-        #X_2 = w_tXw
+        # X_2 = w_tXw
 
         w_tX = torch.matmul(torch.transpose(w_3, dim0=1, dim1=2), X_2)
         w_tXw = torch.matmul(w_tX, w_3)
@@ -213,7 +200,6 @@ class SPDNetwork_2(torch.nn.Module):
         return logits, feat
 
     def update_para(self, lr):
-
         egrad_w1 = self.w_1_p.grad.data.numpy()
         egrad_w2 = self.w_2_p.grad.data.numpy()
         egrad_w3 = self.w_3_p.grad.data.numpy()
@@ -229,7 +215,7 @@ class SPDNetwork_2(torch.nn.Module):
         self.w_2_p.data.copy_(torch.DoubleTensor(new_w_2))
         self.w_3_p.data.copy_(torch.DoubleTensor(new_w_3))
 
-        #self.fc_w.data -= lr * self.fc_w.grad.data
+        # self.fc_w.data -= lr * self.fc_w.grad.data
         # Manually zero the gradients after updating weights
         self.w_1_p.grad.data.zero_()
         self.w_2_p.grad.data.zero_()
@@ -242,89 +228,6 @@ class SPDNetwork_2(torch.nn.Module):
         self.fc_w.grad.data.zero_()
 
 
-
-'''
-
-class SPDNetwork_2(torch.nn.Module):
-
-    def __init__(self):
-        super(SPDNetwork_2, self).__init__()
-
-        self.w_1_p = Variable(torch.randn(32, 16).double(), requires_grad=True)
-        self.w_2_p = Variable(torch.randn(16, 8).double(), requires_grad=True)
-        self.w_3_p = Variable(torch.randn(8, 4).double(), requires_grad=True)
-        self.fc_w = Variable(torch.randn(16, 2).double(), requires_grad=True)
-
-        #self.w_1_p = Variable(nn.init.zeros_(torch.empty(32,16)).double(), requires_grad=True)
-        #self.w_2_p = Variable(nn.init.zeros_(torch.empty(16,8)).double(), requires_grad=True)
-        #self.w_3_p = Variable(nn.init.eye_(torch.empty(8,4)).double(), requires_grad=True)
-        #self.fc_w = Variable(nn.init.zeros_(torch.empty(16,2)).double(), requires_grad=True)
-
-    def forward(self, input):
-        batch_size = input.shape[0]
-        w_1_pc = self.w_1_p.contiguous()
-        w_1 = w_1_pc.view([1, w_1_pc.shape[0], w_1_pc.shape[1]])
-
-        w_2_pc = self.w_2_p.contiguous()
-        w_2 = w_2_pc.view([1, w_2_pc.shape[0], w_2_pc.shape[1]])
-
-        w_3_pc = self.w_3_p.contiguous()
-        w_3 = w_3_pc.view([1, w_3_pc.shape[0], w_3_pc.shape[1]])
-
-        w_tX = torch.matmul(torch.transpose(w_1, dim0=1, dim1=2), input)
-        w_tXw = torch.matmul(w_tX, w_1)
-        X_1 = util.rec_mat_v2(w_tXw)
-        #X_1 = w_tXw 
-
-        w_tX = torch.matmul(torch.transpose(w_2, dim0=1, dim1=2), X_1)
-        w_tXw = torch.matmul(w_tX, w_2)
-        X_2 = util.rec_mat_v2(w_tXw)
-        #X_2 = w_tXw 
-
-        w_tX = torch.matmul(torch.transpose(w_3, dim0=1, dim1=2), X_2)
-        w_tXw = torch.matmul(w_tX, w_3)
-        X_3 = util.log_mat_v2(w_tXw)
-
-        feat = X_3.view([batch_size, -1])  # [batch_size, d]
-        logits = torch.matmul(feat, self.fc_w)  # [batch_size, num_class]
-
-        return logits, feat
-
-    def update_para(self, lr):
-
-        egrad_w1 = self.w_1_p.grad.data.numpy()
-        egrad_w2 = self.w_2_p.grad.data.numpy()
-        egrad_w3 = self.w_3_p.grad.data.numpy()
-        w_1_np = self.w_1_p.data.numpy()
-        w_2_np = self.w_2_p.data.numpy()
-        w_3_np = self.w_3_p.data.numpy()
-
-        new_w_3 = util.update_para_riemann(w_3_np, egrad_w3, lr)
-        new_w_2 = util.update_para_riemann(w_2_np, egrad_w2, lr)
-        new_w_1 = util.update_para_riemann(w_1_np, egrad_w1, lr)
-
-
-        self.w_1_p.data.copy_(torch.DoubleTensor(new_w_1))
-        self.w_2_p.data.copy_(torch.DoubleTensor(new_w_2))
-        self.w_3_p.data.copy_(torch.DoubleTensor(new_w_3))
-
-        #self.fc_w.data -= lr * self.fc_w.grad.data
-        # Manually zero the gradients after updating weights
-        self.w_1_p.grad.data.zero_()
-        self.w_2_p.grad.data.zero_()
-        self.w_3_p.grad.data.zero_()
-
-        return self.fc_w.grad.data
-
-    def second_update_para(self, lr, average_grad):
-        self.fc_w.data -= lr * average_grad
-        self.fc_w.grad.data.zero_()
-
-
-
-
-
-'''
 class SPDNetwork_3(torch.nn.Module):
 
     def __init__(self):
@@ -334,10 +237,10 @@ class SPDNetwork_3(torch.nn.Module):
         self.w_3_p = Variable(torch.randn(4, 4).double(), requires_grad=True)
         self.fc_w = Variable(torch.randn(16, 2).double(), requires_grad=True)
 
-        #self.w_1_p = Variable(nn.init.zeros_(torch.empty(32,16)).double(), requires_grad=True)
-        #self.w_2_p = Variable(nn.init.zeros_(torch.empty(16,8)).double(), requires_grad=True)
-        #self.w_3_p = Variable(nn.init.eye_(torch.empty(8,4)).double(), requires_grad=True)
-        #self.fc_w = Variable(nn.init.zeros_(torch.empty(16,2)).double(), requires_grad=True)
+        # self.w_1_p = Variable(nn.init.zeros_(torch.empty(32,16)).double(), requires_grad=True)
+        # self.w_2_p = Variable(nn.init.zeros_(torch.empty(16,8)).double(), requires_grad=True)
+        # self.w_3_p = Variable(nn.init.eye_(torch.empty(8,4)).double(), requires_grad=True)
+        # self.fc_w = Variable(nn.init.zeros_(torch.empty(16,2)).double(), requires_grad=True)
 
     def forward(self, input):
         batch_size = input.shape[0]
@@ -350,7 +253,7 @@ class SPDNetwork_3(torch.nn.Module):
         w_tX = torch.matmul(torch.transpose(w_1, dim0=1, dim1=2), input)
         w_tXw = torch.matmul(w_tX, w_1)
         X_1 = util.rec_mat_v2(w_tXw)
-        #X_1 = w_tXw 
+        # X_1 = w_tXw
 
         w_tX = torch.matmul(torch.transpose(w_3, dim0=1, dim1=2), X_1)
         w_tXw = torch.matmul(w_tX, w_3)
@@ -362,7 +265,6 @@ class SPDNetwork_3(torch.nn.Module):
         return logits, feat
 
     def update_para(self, lr):
-
         egrad_w1 = self.w_1_p.grad.data.numpy()
         egrad_w3 = self.w_3_p.grad.data.numpy()
         w_1_np = self.w_1_p.data.numpy()
@@ -371,11 +273,10 @@ class SPDNetwork_3(torch.nn.Module):
         new_w_3 = util.update_para_riemann(w_3_np, egrad_w3, lr)
         new_w_1 = util.update_para_riemann(w_1_np, egrad_w1, lr)
 
-
         self.w_1_p.data.copy_(torch.DoubleTensor(new_w_1))
         self.w_3_p.data.copy_(torch.DoubleTensor(new_w_3))
 
-        #self.fc_w.data -= lr * self.fc_w.grad.data
+        # self.fc_w.data -= lr * self.fc_w.grad.data
         # Manually zero the gradients after updating weights
         self.w_1_p.grad.data.zero_()
         self.w_3_p.grad.data.zero_()
